1dd77da7d0cff8456faa743144c98c955f0f2309
alexandre <alexandre020602@gmail.com>
2025-07-17 15:09:44 -0300

    type para os tweets

---
+++ src/Types/NewTweets/newTweets.ts
@@ -0,0 +1,12 @@
+export type newTweet = {
+    id: string,
+    name: string,
+    username: string
+    content: string
+    avatar: string
+    time: string
+    image?: string | null
+    likes?: number
+    retweets?: number
+    comments?: number
+}
