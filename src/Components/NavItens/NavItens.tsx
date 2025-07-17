b199cec77d0e1ac223d3b30f157dac2ac07f5896
alexandre <alexandre020602@gmail.com>
2025-07-17 00:21:11 -0300

    finalizando sideBar e navItems

--- src/Components/NavItens/NavItens.tsx
+++ src/Components/NavItens/NavItens.tsx
@@ -11,8 +11,8 @@ const NavItens = ({icon, text}: Props) => {
   return (
 
     <div className="flex items-center p-3 rounded-full cursor-pointer hover:bg-gray-600 transform duration-200 ease-out">
-        <FontAwesomeIcon icon={icon} className="text-2xl md-4" />
-        <span className="text-xl hidden xl:inline">{text}</span>
+        <FontAwesomeIcon icon={icon} className="text-2xl mr-4" />
+        <span className="text-md hidden xl:inline">{text}</span>
     </div>
   )
 }
