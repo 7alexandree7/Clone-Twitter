9507d32ff4be029b6802f6b2104a71d8ed3e1571
alexandre <alexandre020602@gmail.com>
2025-07-17 02:56:44 -0300

    trabalhando no componente twitter form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Header from "./Components/Header/Header"
 import SideBar from "./Components/SideBar/SideBar"
+import TwitterForm from "./Components/TwitterForm/TwitterForm"
 
 function App() {
 
   return (
     <div className="flex max-w-7xl m-auto">
       <SideBar />
-      <main className="flex flex-grow border-l border-r border-gray-700 max-w-xl">
+      <main className="flex-grow border-l border-r border-gray-700 max-w-xl">
         <Header />
+        <TwitterForm />
       </main>
     </div>
   )
---
+++ src/Components/TwitterForm/TwitterForm.tsx
@@ -0,0 +1,45 @@
+import { faCalendarAlt, faChartBar, faFilm, faImage, faMapMarkedAlt, faSmile } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+type icons = {
+    id: number,
+    icon: IconDefinition
+}
+
+const TwitterForm = () => {
+
+    const iconsComponents: icons[] = [
+
+        { id: 1, icon: faImage },
+        { id: 2, icon: faFilm },
+        { id: 3, icon: faChartBar },
+        { id: 4, icon: faSmile },
+        { id: 5, icon: faCalendarAlt },
+        { id: 6, icon: faMapMarkedAlt },
+
+    ]
+
+    return (
+        <div className="border-b border-gray-700 w-full">
+            <textarea
+                className="bg-transparent w-full outline-none resize-none p-2"
+                placeholder="What's happening?"
+                maxLength={280}
+            />
+            <div className="flex items-center justify-between py-2">
+                <div className="flex space-x-4">
+                    {iconsComponents.map((icon) => {
+                        return (
+                            <FontAwesomeIcon key={icon.id} icon={icon.icon} className="text-blue-400 cursor-pointer" />
+                        )
+                    })}
+                </div>
+                <button className="bg-blue-400 text-white rounded-full font-bold px-6 py-2 hover:bg-blue-600 traansition duration-200 mr-4">Tweet</button>
+
+            </div>
+        </div>
+    )
+}
+
+export default TwitterForm
