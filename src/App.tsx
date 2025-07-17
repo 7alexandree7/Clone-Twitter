0ca4f4ff49eed2b260feacb2dc6268cd782ef0ce
alexandre <alexandre020602@gmail.com>
2025-07-17 02:11:37 -0300

    componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Header from "./Components/Header/Header"
 import SideBar from "./Components/SideBar/SideBar"
 
 function App() {
 
   return (
     <div className="flex max-w-7xl m-auto">
-    <SideBar />
+      <SideBar />
+      <main className="flex flex-grow border-l border-r border-gray-700 max-w-xl">
+        <Header />
+      </main>
     </div>
   )
 }
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <header className="sticky top-0 z-10 bg-[#15202b] bg-opacity-80 backdrop-blur-sm">
+            <h2 className="px-4 py-3 text-xl font-bold">For You</h2>
+        </header>
+    )
+}
+
+export default Header
