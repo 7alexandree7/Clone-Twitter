4e20199c583bf6e3a5dfd18d3d5cbdb86a856bb1
alexandre <alexandre020602@gmail.com>
2025-07-18 16:27:22 -0300

    finalização do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,26 @@ import SideBar from "./Components/SideBar/SideBar"
 import Tweet from "./Components/Tweet/Tweet"
 import TwitterForm from "./Components/TwitterForm/TwitterForm"
 import { v4 as uuidv4 } from 'uuid';
-import { getAvatar } from "./utils/GenerateImages/generateImages";
-import { getRandomImage } from "./utils/GenerateImages/generateImages";
+import { getAvatar, getRandomImage } from "./utils/GenerateImages/generateImages";
 import { useState, useEffect } from "react";
 import type { newTweet } from "./Types/NewTweets/newTweets"
+import generatePosts from "./utils/GeneratePosts/generatePosts";
+import Aside from "./Components/Aside/Aside";
 
 function App() {
-
- 
   const [tweets, setTweets] = useState<newTweet[]>([]);
 
   useEffect(() => {
-    console.table(tweets)
-  },[tweets])
+    const interval = setInterval(() => {
+      addNewTweet(generatePosts())
+    }, 60000);
 
-  const addNewTweet = (content: string, includeImage: boolean = false) => {
+    return () => clearInterval(interval);
+  }, [])
 
+
+
+  const addNewTweet = (content: string) => {
     const newTweet: newTweet = {
       id: uuidv4(),
       name: "Unfast",
@@ -26,13 +30,24 @@ function App() {
       avatar: getAvatar(`unfast${Math.floor(Math.random() * 100)}@email.com`),
       content,
       time: new Date().toLocaleString(['pt-BR'], { hour: 'numeric', minute: 'numeric' }),
-      image: includeImage ? getRandomImage() : null,
+      image: getRandomImage(),
       likes: 0,
       retweets: 0,
-      comments: 0
-    }
-    setTweets((prevTweets: newTweet[]) => [newTweet, ...prevTweets])
-  }
+      comments: 0,
+    };
+    setTweets((prevTweets) => [newTweet, ...prevTweets]);
+  };
+
+
+  const handleTweetAction = (id: string, action: "likes" | "comments" | "retweets") => {
+    setTweets(prev =>
+      prev.map(tweet =>
+        tweet.id === id
+          ? { ...tweet, [action]: (tweet[action] ?? 0) + 1 }
+          : tweet
+      )
+    );
+  };
 
 
   return (
@@ -40,15 +55,20 @@ function App() {
       <SideBar />
       <main className="flex-grow border-l border-r border-gray-700 max-w-xl">
         <Header />
-        <TwitterForm onTweet={(content: string) => addNewTweet(content, Math.random() > 0.5 )} />
+        <TwitterForm onTweet={addNewTweet} />
         <div>
           {tweets.map((tweet) => (
-            <Tweet key={tweet.id} tweet={tweet} />
+            <Tweet
+              key={tweet.id}
+              tweet={tweet}
+              onAction={(action) => handleTweetAction(tweet.id, action)}
+            />
           ))}
         </div>
       </main>
+      <Aside />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Tweet/Tweet.tsx
+++ src/Components/Tweet/Tweet.tsx
@@ -1,13 +1,69 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import type { newTweet } from "../../Types/NewTweets/newTweets"
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons/faEllipsisH"
+import { faChartBar, faComment, faHeart, faRetweet, faUpload } from "@fortawesome/free-solid-svg-icons"
 
-const Tweet = ({tweet}: {tweet: newTweet}) => {
-  return (
-    <div className="border-b border-gray-800 hover:bg-gray-700 transition duration-200 ease-in-out">
-      <div>
-        <img src={tweet.avatar} alt="avatar do perfil do usuário" />
-      </div>
-    </div>
-  )
+interface PropsTweet {
+  tweet: newTweet;
+  onAction: (action: "likes" | "comments" | "retweets") => void;
 }
 
-export default Tweet
+
+const Tweet = ({ tweet, onAction }: PropsTweet) => {
+  return (
+    <div className="border-b border-gray-800 hover:bg-gray-700 transition duration-200 ease-in-out p-4">
+      <div className="flex space-x-3">
+        <img className="w-12 h-12 rounded-full" src={tweet.avatar} alt="avatar do perfil do usuário" />
+
+        <div className="flex-1">
+
+          <div className="flex items-center justify-between py-2 px-4">
+            <div>
+              <span className="font-bold">{tweet.name}</span>
+              <span className="text-gray-500 ml-2">@{tweet.username}</span>
+              <span className="text-gray-500 ml-2">{tweet.time}</span>
+            </div>
+            <FontAwesomeIcon icon={faEllipsisH} className="text-gray-500 cursor-pointer" />
+          </div>
+
+          <p className="px-4 text-[15px] xl:text-base">{tweet.content}</p>
+          {tweet.image && (
+            <img
+              src={tweet.image}
+              alt="Imagem do tweet"
+              className="max-w-full mt-3 rounded-xl h-auto"
+            />
+          )}
+
+          <div className="flex justify-between mt-4 text-gray-50">
+            <div className="flex items-center cursor-pointer hover:text-blue-400 transition duration-200 ease-out">
+              <FontAwesomeIcon onClick={() => onAction("comments")} icon={faComment} />
+              <span className="ml-2">{tweet.comments}</span>
+            </div>
+
+            <div className="flex items-center cursor-pointer hover:text-green-400 transition duration-200 ease-out">
+              <FontAwesomeIcon onClick={() => onAction("retweets")} icon={faRetweet} />
+              <span className="ml-2">{tweet.retweets}</span>
+            </div>
+
+            <div className="flex items-center cursor-pointer hover:text-red-400 transition duration-200 ease-out">
+              <FontAwesomeIcon onClick={() => onAction("likes")} icon={faHeart} />
+              <span className="ml-2">{tweet.likes}</span>
+            </div>
+
+            <div className="flex items-center cursor-pointer hover:text-blue-400 transition duration-200 ease-out">
+              <FontAwesomeIcon icon={faChartBar} />
+            </div>
+
+            <div className="flex items-center cursor-pointer hover:text-blue-400 transition duration-200 ease-out mr-2">
+              <FontAwesomeIcon icon={faUpload} />
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tweet;
